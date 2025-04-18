d060c252c7fc80ae9a6558f13d47400c2249dfbd
coder <1186977762@qq.com>
2025-04-18 19:10:10 +0800

    将RAG包装为类

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ## 关于```faiss```库
 
 - 索引方式的简单介绍：[知乎链接](https://zhuanlan.zhihu.com/p/530958094)
+- 不同索引方法的比较：[CSDN文章](https://blog.csdn.net/uncle_ll/article/details/139819001)
--- SimpleRAG.py
+++ SimpleRAG.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 import numpy as np
 import faiss
-import numpy as np
 from typing import List
+from datasets import Dataset
+import datasets
+import os
 
 
 class EmbeddingModel:
@@ -35,31 +37,75 @@ class EmbeddingModel:
         return np.vstack(embeddings)
 
 
+class RAG:
+    def __init__(self, documents):
+        self.embedding = EmbeddingModel()
+        self.file_path = "./storage/index.db"
+        
+        embeddings = self.embedding(documents)
+        index = faiss.IndexFlatIP(embeddings.shape[1])
+        index.add(embeddings)
+        self.index = index
+        # faiss.write_index(index, self.file_path)
+    
+    def __call__(self, *args, **kwargs):
+        return search(*args, **kwargs)
+    
+    # def load(self):
+    #     return faiss.read_index(self.file_path)
+    #
+    # def create(self, text: List[str]):
+    #     pass
+    
+    def search(self, query, k=2):
+        if isinstance(query, str):
+            query = [query]
+        query_emb = self.embedding(query)
+        
+        return self.index.search(query_emb, k=k)
+
+
+def eval():
+    pass
+
+
+def load_dataset():
+    """
+    return a dataset of 1000 lines. As for its content:
+    Dataset({
+        features: ['question', 'answer', 'docs'],
+        num_rows: 1000
+    })
+    """
+    base_dir = "/home/coder/Documents/CodeAndFiles/Corpus"
+    cache_dir = os.path.join(base_dir, "cache")
+    
+    dataset = datasets.load_dataset(
+        path=os.path.join(base_dir, "sciq"),
+        cache_dir=cache_dir,
+    )['test']
+    dataset = dataset.remove_columns(['distractor3', 'distractor1', 'distractor2'])
+    dataset = dataset.rename_columns({"correct_answer": "answer", "support": "docs"})
+    return dataset
+
+
+def rag_display():
+    documents = data["docs"]
+    rag = RAG(data['docs'])
+    
+    query = ["What is a phosphatase?", "what is the study of ecosystems?"]
+    
+    Distance, Index = rag.search(query, k=5)
+    
+    print("最相关的文档:")
+    for query_index, item in enumerate(zip(Index, Distance)):
+        doc_index_list, score_list = item
+        print(f"query {query_index + 1}:{query[query_index]}", "=====", sep='\n')
+        for doc_index, score in zip(doc_index_list, score_list):
+            print(f"{score:.3f}:{documents[doc_index]}")
+
+
 if __name__ == "__main__":
     embedding = EmbeddingModel()
-    
-    documents = [
-        "Faiss is a library for efficient similarity search",
-        "Facebook AI Research developed Faiss",
-        "Approximate nearest neighbor search is useful for large datasets",
-        "Vector databases power modern search applications"
-    ]
-    
-    doc_embeddings = embedding(documents)
-    
-    # 构建索引
-    index = faiss.IndexFlatIP(doc_embeddings.shape[1])
-    index.add(doc_embeddings)
-    
-    query = ["What is Faiss?", "what is a vector database?"]
-    query_embedding = embedding(query)
-    
-    D, I = index.search(query_embedding, k=2)
-    
-    # 输出结果
-    print("最相关的文档:")
-    for query_index, item in enumerate(zip(I, D)):
-        doc_list, score_list = item
-        print(f"query {query_index + 1}:{query[query_index]}", "=====", sep='\n')
-        for doc_index, score in zip(doc_list, score_list):
-            print(f"{score:.3f}:{documents[doc_index]}")
+    data = load_dataset()
+    # rag_display()
